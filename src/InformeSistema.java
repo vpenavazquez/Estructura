36d7d8f027069815e551decca42f693b9994b216
vpenavazquez <vpenavazquez@danielcastelao.org>
2026-09-15 10:12:33 +0200

    primer commit

--- src/InformeSistema.java
+++ src/InformeSistema.java
@@ -1,5 +1,8 @@
 public class InformeSistema {
 
-
+    public static void main (String[] args ){
+        int procesadores = Runtime.getRuntime().availableProcessors();
+        System.out.println("Numero de procesadores:" + procesadores);
+    }
 
 }
